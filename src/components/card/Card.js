d20731b02f5a71eeeaf9fd8110fa95843066ffc2
pwwpche <pwwpche@gmail.com>
2019-03-22 00:06:06 -0700

    Add logo

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './card.css';
+import logo from './logo_hwat.png';
 
 export default class Card extends Component {
     render() {
@@ -7,6 +8,9 @@ export default class Card extends Component {
             <div className='App'>
                 <div class="login-card">
                     <h1>{this.props.title}</h1>
+                    <div class="image-container">
+                        <img src={logo} alt="logo"></img>
+                    </div>
                     {this.props.children}
                 </div>
             </div>
